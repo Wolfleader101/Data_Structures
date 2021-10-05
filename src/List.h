2520c8ab535dd07646749b23f71d3535b158c53f
Wolfleader <james.morgan@iinet.net.au>
2021-10-05 10:45:10 +0800

    resize, get and insert

--- src/List.h
+++ src/List.h
@@ -11,28 +11,53 @@ private:
 	size_t m_Size;
 	uint32_t m_Length;
 
+	// resize method from cppref
+	void resize(size_t newSize)
+	{
+		if (newSize == 0) { // this check is not strictly needed, 
+			std::free(m_Data);  // but zero-size realloc is deprecated in C
+			m_Data = nullptr;
+		}
+		else {
+			if (void* mem = std::realloc(m_Data, newSize))
+			{
+				m_Data = static_cast<T*>(mem);
+			}
+			else
+			{
+				throw std::bad_alloc();
+			}
+		}
+	}
+
 public:
 	List()
-		: m_Size(0), m_Length(0), m_Data(nullptr){}
+		: m_Length(0), m_Size(0), m_Data(nullptr){}
 
-	List(uint32_t length) {
-		m_Length = length;
-		m_Size = m_Length * sizeof(T);
-		m_Data = (T*)malloc(m_Size);
-	}
+	List(uint32_t length) 
+		: m_Length(length), m_Size(length * sizeof(T)), m_Data((T*)std::malloc(m_Size)){}
 
 	~List() {
-		free(m_Data);
+		std::free(m_Data);
 	}
 
-	size_t size() const
-	{
-		return m_Size;
-	}
+	size_t size() const	{return m_Size;}
 
-	uint32_t length() const
+	uint32_t length() const	{return m_Length;}
+
+	uint32_t insert(T el)
 	{
+		m_Length++;
+		m_Size = m_Length * sizeof(T);
+		resize(m_Size);
+		m_Data[m_Length] = el;
+
 		return m_Length;
+	}
+
+	T get(uint32_t index)
+	{
+		return m_Data[index];
 	}
 
 };
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@ int main()
 	List<int> array = List<int>(5);
 	std::cout << "Length: " << array.length() << std::endl;
 	std::cout << "Size: " << array.size() << std::endl;
+	uint32_t insertion = array.insert(2);
+	std::cout << "Length: " << array.length() << std::endl;
+	std::cout << "Size: " << array.size() << std::endl;
+	std::cout << "last el: " << array.get(6) << std::endl;
 
 	std::cin.get();
 }
